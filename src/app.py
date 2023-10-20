1166842741dc2715afa7cb8e8263abbcba8ea954
yyyxam <maximilian.wenger.goerlitz@gmail.com>
2023-10-20 20:10:24 +0200

    restructuring files

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
 # IMPORTS
 import json
-import os
-from collections import Counter
 
-from mala_inference import run_mala_prediction
+from assets.mala_inference import run_mala_prediction
 import dash
 import dash_bootstrap_components as dbc
 
@@ -29,7 +27,7 @@ ATOM_LIMIT = 200
 # TODO: implement patching so that figures are updated, nor recreated
 # as in: https://dash.plotly.com/partial-properties
 
-models = json.load(open("models/model_list.json"))
+models = json.load(open("../models/model_list.json"))
 # "label" is the label visible in the apps dropdown ; "value"  is the value passed to the inference script. Ranges are to be surrounded by []
 
 
