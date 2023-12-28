1b49fb65c346b6865d94585eaee2074a9d6969fb
Lenz Fiedler <l.fiedler@hzdr.de>
2023-12-28 10:58:46 +0100

    Implemented functions to save inference output to

--- src/utils/mala_inference.py
+++ src/utils/mala_inference.py
@@ -99,3 +99,89 @@ def run_mala_prediction(atoms_to_predict, model_and_temp,
         else:
             results["total_energy"] = 0.0
         return results
+
+
+def save_density_to_file(results, file_name):
+    """
+    Save the density of an inference back to file.
+
+    Function returns nothing, but writes a single file containing the density.
+
+    Parameters
+    ----------
+    results : dict
+        A dictionary that contains:
+            "band_energy": The band energy, which can be useful for specific
+                           analysis.
+
+            "total_energy": The total energy of the system, which is the
+                            principal quantity to investigate when e.g.
+                            optimizing the atomic geometry of a system.
+
+            "density": Electronic density as a 3D numpy array, which
+                       gives information about the electronic distribution.
+
+            "density_of_states": The density of states (DOS) which is an
+                                 important property in analyzing electrical
+                                 and optical phenomena.
+
+            "energy_grid": The energy grid on which the DOS is supposed
+                           to be plotted.
+
+    file_name : string
+        Name of the file in which the density will be saved.
+    """
+    parameters = mala.Parameters()
+    density_calculator = mala.Density(parameters)
+    density_calculator.voxel = results["voxel"]
+    density_calculator.atoms = results["atoms"]
+    density_calculator.density = results["density"]
+    density_calculator.grid_dimensions = results["grid_dimensions"]
+    density_calculator.write_to_cube(file_name)
+
+
+def save_dos_to_file(results, dos_file_name, energy_grid_file_name):
+    """
+    Save the density of an inference back to file.
+
+    Function returns nothing, but writes two numpy files,
+    containing DOS and energy grid.
+
+    Parameters
+    ----------
+    results : dict
+        A dictionary that contains:
+            "band_energy": The band energy, which can be useful for specific
+                           analysis.
+
+            "total_energy": The total energy of the system, which is the
+                            principal quantity to investigate when e.g.
+                            optimizing the atomic geometry of a system.
+
+            "density": Electronic density as a 3D numpy array, which
+                       gives information about the electronic distribution.
+
+            "density_of_states": The density of states (DOS) which is an
+                                 important property in analyzing electrical
+                                 and optical phenomena.
+
+            "energy_grid": The energy grid on which the DOS is supposed
+                           to be plotted.
+
+    dos_file_name : string
+        Name of the file in which the DOS will be saved.
+
+    energy_grid_file_name : string
+        Name of the file in which the energy grid will be saved.
+    """
+
+    parameters = mala.Parameters()
+    dos_calculator = mala.DOS(parameters)
+    # A bit hacky, the general write function will be used for
+    # both the actual DOS as well as the energy grid.
+    dos_calculator.density_of_states = results["density_of_states"]
+    dos_calculator.write_to_numpy_file(dos_file_name)
+    dos_calculator.density_of_states = results["energy_grid"]
+    dos_calculator.write_to_numpy_file(energy_grid_file_name)
+
+
